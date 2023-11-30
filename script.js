6c8f7b660283edd70f3157e433f40c4694f94774
Stan Man <skater4@yandex.ru>
2023-11-30 22:57:24 +0500

    Init commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let lastMessage = ''
+setInterval(function(){
+	var assistantMessages = document.querySelectorAll('[data-message-author-role="assistant"]');
+	var lastAssistantMessage = assistantMessages[assistantMessages.length - 1];
+
+	if (lastAssistantMessage && lastAssistantMessage.querySelector('.markdown')) {
+		lastMessage = lastAssistantMessage.querySelector('.markdown').innerText;
+	}
+
+	if (lastAssistantMessage && lastAssistantMessage.querySelector('.border-red-500') && lastMessage != '') {
+		console.log('fix ' + lastMessage);
+		// Очищаем элемент
+		lastAssistantMessage.innerHTML = '';
+
+		// Создаем новый div с классом 'markdown' и добавляем его
+		var markdownDiv = document.createElement('div');
+		markdownDiv.className = 'markdown prose w-full break-words dark:prose-invert light';
+		markdownDiv.textContent = lastMessage; // Замените этот текст на нужный вам текст
+
+		lastAssistantMessage.appendChild(markdownDiv);
+	}
+}, 100);
